9d0941476dabf6dea6eebc7a8246dfa1683b8a03
Ben Coleman <colemanb@moravian.edu>
2022-02-22 08:58:44 -0500

    Remove old code

--- collector/collector.py
+++ collector/collector.py
@@ -15,7 +15,7 @@ r = redis.Redis(host=host, port=port)
 while True:
     url = 'https://data.cdc.gov/resource/9mfq-cb36.json?submission_date=2022-01-15'
     results = requests.get(url).json()
-    confirmed = sum([int(result['tot_cases']) for result in results])    response = requests.get('https://covid19-api.com/totals')
+    confirmed = sum([int(result['tot_cases']) for result in results])
 
     r.set('confirmed', confirmed)
     time.sleep(15 * 60)
