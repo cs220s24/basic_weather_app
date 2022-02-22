afcaf50e353bfc7d6e02c3c52891ed71b2565f21
Ben Coleman <colemanb@moravian.edu>
2022-02-22 07:46:43 -0500

    Change to CDC server

--- collector/collector.py
+++ collector/collector.py
@@ -13,8 +13,9 @@ port = os.getenv('REDIS_PORT')
 r = redis.Redis(host=host, port=port)
 
 while True:
-    response = requests.get('https://covid19-api.com/totals')
-    confirmed = response.json()[0]['confirmed']
+    url = 'https://data.cdc.gov/resource/9mfq-cb36.json?submission_date=2022-01-15'
+    results = requests.get(url).json()
+    confirmed = sum([int(result['tot_cases']) for result in results])    response = requests.get('https://covid19-api.com/totals')
 
     r.set('confirmed', confirmed)
     time.sleep(15 * 60)
