320771b8591e660ba203135451821cf21c7ae6be
Ben Coleman <colemanb@moravian.edu>
2024-04-03 20:26:23 -0400

    check for .env files

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,14 @@
-sudo yum install redis6
+if [ ! -f /collector/.env ]; then
+    echo "Create collector/.env before deploying"
+    exit
+fi
+
+if [ ! -f /server/.env ]; then
+    echo "Create server/.env before deploying"
+    exit
+fi
+
+sudo yum install -y redis6
 sudo systemctl enable redis6
 sudo systemctl start redis6
 python3 -m venv collector/.venv
