9207a165e9149cb839cc055a3ff6cfd908cf0f05
Ben Coleman <colemanb@moravian.edu>
2024-04-03 20:35:39 -0400

    add deployment for server

--- deploy.sh
+++ deploy.sh
@@ -16,4 +16,9 @@ collector/.venv/bin/pip install -r collector/requirements.txt
 sudo cp collector/weather_collector.service /etc/systemd/system
 sudo systemctl enable weather_collector
 sudo systemctl start weather_collector
+python3 -m venv server/.venv
+server/.venv/bin/pip install -r server/requirements.txt
+sudo cp server/weather_server.service /etc/systemd/system
+sudo systemctl enable weather_server
+sudo systemctl start weather_server
 
